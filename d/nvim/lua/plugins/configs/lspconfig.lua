e8e8921e5df3d1117f0934498240aed764382f32
Nathan Bardavid <nbardavi@bess-f1r4s10.clusters.42paris.fr>
2024-05-07 12:33:40 +0200

    d

---
+++ d/nvim/lua/plugins/configs/lspconfig.lua
@@ -0,0 +1,82 @@
+dofile(vim.g.base46_cache .. "lsp")
+require "nvchad.lsp"
+
+local M = {}
+local utils = require "core.utils"
+
+-- export on_attach & capabilities for custom lspconfigs
+
+M.on_attach = function(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  utils.load_mappings("lspconfig", { buffer = bufnr })
+
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad.signature").setup(client)
+  end
+
+  if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
+M.capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
+}
+
+require("noice").setup {
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      -- override the default lsp markdown formatter with Noice
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      -- override the lsp markdown formatter with Noice
+      ["vim.lsp.util.stylize_markdown"] = true,
+      -- override cmp documentation with Noice (needs the other options to work)
+      ["cmp.entry.get_documentation"] = true,
+    },
+    hover = { enabled = true }, -- <-- HERE!
+    signature = { enabled = true }, -- <-- HERE!
+  },
+}
+require("lspconfig").lua_ls.setup {
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
+
+return M
