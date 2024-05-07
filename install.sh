545b6b480bc6036b9db202215bdee41753e72fc9
Nathan Bardavid <nbardavi@bess-f1r4s10.clusters.42paris.fr>
2024-05-07 12:04:04 +0200

    d

--- install.sh
+++
@@ -1,7 +0,0 @@
-tar -xvf nvim-linux64.tar.xz
-tar -xvf nvim.tar.xz
-mv -R nvim-linux64 ~/
-mv -R ~/.config
-echo "alias vim='~/nvim-linux64/bin/nvim'" >> ~/.zshrc
-echo "alias nvimconfig='cd ~/.config/nvim/lua/custom/'"
-exec zsh
