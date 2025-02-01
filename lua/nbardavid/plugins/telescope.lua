cc2a083688e7763efe364a59804d559dd3b9c373
Nathan Bardavid <nbabardavid@gmail.com>
2025-02-01 11:25:40 +0100

    replaced tssserver by ts_ls (new) and added handling of ignore.telescope to ignore directory or file in telescope localy

--- lua/nbardavid/plugins/telescope.lua
+++ lua/nbardavid/plugins/telescope.lua
@@ -10,11 +10,26 @@ return {
     config = function()
         local telescope = require("telescope")
         local actions = require("telescope.actions")
-        local transform_mod = require("telescope.actions.mt").transform_mod
+
+        local function get_local_ignore_patterns()
+            local ignore_file = vim.fn.getcwd() .. "/ignore.telescope"
+            local patterns = {}
+
+            if vim.fn.filereadable(ignore_file) == 1 then
+                for line in io.lines(ignore_file) do
+                    if line ~= "" then
+                        table.insert(patterns, line)
+                    end
+                end
+            end
+
+            return patterns
+        end
 
         telescope.setup({
             defaults = {
                 path_display = { "smart" },
+                file_ignore_patterns = get_local_ignore_patterns(), 
                 mappings = {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -30,7 +45,6 @@ return {
         local keymap = vim.keymap
 
         keymap.set("n", "<leader>ff", function() vim.cmd("Telescope find_files") end, { desc = "Fuzzy find files in cwd" })
-        -- keymap.set("n", "<leader>fr", function() vim.cmd("Telescope oldfiles") end, { desc = "Fuzzy find recent files" })
         keymap.set("n", "<leader>fw", function() vim.cmd("Telescope live_grep") end, { desc = "Find word in cwd" })
         keymap.set("n", "<leader>fc", function() vim.cmd("Telescope grep_string") end, { desc = "Find string under cursor in cwd" })
         keymap.set("n", "<leader>ft", function() vim.cmd("TodoTelescope") end, { desc = "Find todos" })
