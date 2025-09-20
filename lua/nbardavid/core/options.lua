3145b7c9b4a17ce3517a46e35d4edd826b14e4ce
Nathan Bardavid <nbabardavid@gmail.com>
2025-09-20 13:30:18 +0200

    switch from require('lspconfig') to the new version

--- lua/nbardavid/core/options.lua
+++ lua/nbardavid/core/options.lua
@@ -30,14 +30,6 @@ vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
     end,
 })
 
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-})
-
 opt.relativenumber = true
 opt.number = true
 
--- lua/nbardavid/plugins/lsp/lspconfig.lua
+++ lua/nbardavid/plugins/lsp/lspconfig.lua
@@ -6,105 +6,91 @@ return {
         { "folke/neodev.nvim", opts = {} }, -- Enhanced Lua LSP for Neovim
     },
     config = function()
-        local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local mason_lspconfig = require("mason-lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
         local capabilities = cmp_nvim_lsp.default_capabilities()
         local keymap = vim.keymap
 
-        --C/C++
-        lspconfig.clangd.setup({
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+        })
+
+        local function on_attach(client, bufnr)
+            local opts = { buffer = bufnr, silent = true }
+            keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        end
+
+        vim.lsp.config("clangd", {
             capabilities = capabilities,
+            on_attach = on_attach,
         })
-
-        --Lua
-        lspconfig.lua_ls.setup({
+        vim.lsp.config("lua_ls", {
             capabilities = capabilities,
+            on_attach = on_attach,
         })
-
-        lspconfig.qmlls.setup({
-            cmd = { "qmlls6" },
-            capabilities = capabilities,
-            cmd_env  = {
-                QML_IMPORT_PATH = vim.fn.expand("~/.local/share/quickshell/modules"),
-                QML2_IMPORT_PATH = vim.fn.expand("~/.local/share/quickshell/modules"),
-            }
-        })
-
-        -- Filetype detection for .frag and .vert
-        vim.filetype.add({
-            extension = {
-                frag = "glsl",
-                vert = "glsl",
-            },
-        })
-
-        -- Nix LSP Config
-        lspconfig.nixd.setup({
-            capabilities = capabilities,
-            root_dir = function(fname)
-                local util = require("lspconfig.util")
-                return util.root_pattern("flake.nix", "default.nix", "shell.nix")(fname)
-                    or "/etc/nixos"
-            end,
-            settings = {
-                nixd = {
-                    nixpkgs = {
-                        expr = "import <nixpkgs> {}",
-                    },
-                    formatting = {
-                        command = { "alejandra" },
-                    },
-                    options = {
-                        nixos = {
-                            expr = 'with import <nixpkgs/nixos> { configuration = {}; }; options',
-                        },
-                        -- home_manager = {
-                        --     expr = 'let hm = import <home-manager>; in (hm.lib.homeManagerConfiguration { pkgs = import <nixpkgs> {}; modules = []; }).options'
-                        -- },
-                    },
-                },
-            },
-        })
-
-        -- Keybindings for LSP
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf, silent = true }
-
-                opts.desc = "Show LSP definitions"
-                keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-
-                opts.desc = "Show hover documentation"
-                keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            end,
-        })
-
-        -- Capabilities for autocompletion
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+        -- vim.lsp.config("qmlls", {
+        --     cmd = { "qmlls6" },
+        --     capabilities = capabilities,
+        --     on_attach = on_attach,
+        --     cmd_env = {
+        --         QML_IMPORT_PATH = vim.fn.expand("~/.local/share/quickshell/modules"),
+        --         QML2_IMPORT_PATH = vim.fn.expand("~/.local/share/quickshell/modules"),
+        --     },
+        -- })
+        -- vim.lsp.config("nixd", {
+        --     capabilities = capabilities,
+        --     on_attach = on_attach,
+        --     root_dir = function(fname)
+        --         local util = require("lspconfig.util")
+        --         return util.root_pattern("flake.nix", "default.nix", "shell.nix")(fname)
+        --             or "/etc/nixos"
+        --     end,
+        --     settings = {
+        --         nixd = {
+        --             nixpkgs = {
+        --                 expr = "import <nixpkgs> {}",
+        --             },
+        --             formatting = {
+        --                 command = { "alejandra" },
+        --             },
+        --             options = {
+        --                 nixos = {
+        --                     expr = 'with import <nixpkgs/nixos> { configuration = {}; }; options',
+        --                 },
+        --             },
+        --         },
+        --     },
+        -- })
 
         mason_lspconfig.setup_handlers({
             function(server_name)
-                if server_name == "pyright" then
-                    lspconfig.pyright.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            python = {
-                                analysis = {
-                                    typeCheckingMode = "basic",
-                                    autoSearchPaths = true,
-                                    useLibraryCodeForTypes = true
-                                },
-                                pythonPath = "~/.venv/nvim/bin/python"
-                            }
-                        }
-                    })
-                else
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end
+                vim.lsp.config(server_name, {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+                vim.lsp.enable(server_name)
+            end,
+            ["pyright"] = function()
+                vim.lsp.config("pyright", {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = {
+                        python = {
+                            analysis = {
+                                typeCheckingMode = "basic",
+                                autoSearchPaths = true,
+                                useLibraryCodeForTypes = true,
+                            },
+                            pythonPath = "~/.venv/nvim/bin/python",
+                        },
+                    },
+                })
+                vim.lsp.enable("pyright")
             end,
         })
     end,
