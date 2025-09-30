3940beb7b61bc79abc365e709cf2d7c6f4fb04ed
Bardavid Nathan <nbabardavid@gmail.com>
2025-09-30 10:52:00 +0200

    added relative path in incline

--- lua/nbardavid/plugins/incline.lua
+++ lua/nbardavid/plugins/incline.lua
@@ -1,8 +1,22 @@
 return {
     'b0o/incline.nvim',
-    config = function()
-        require('incline').setup()
-    end,
-    -- Optional: Lazy load Incline
     event = 'VeryLazy',
+    config = function()
+        local helpers = require 'incline.helpers'
+        local devicons = require 'nvim-web-devicons'
+        require('incline').setup {
+            window = {
+                padding = 0,
+                margin = { horizontal = 0 },
+            },
+            render = function(props)
+                local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':.')
+                return {
+                    ' ',
+                    filename,
+                    ' ',
+                }
+            end
+        }
+    end,
 }
