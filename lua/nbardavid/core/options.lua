0da23af2767814de3a614345a2f55ac07bfba30c
Bardavid Nathan <nbabardavid@gmail.com>
2025-11-08 12:22:48 +0100

    updated

--- lua/nbardavid/core/options.lua
+++ lua/nbardavid/core/options.lua
@@ -1,44 +1,32 @@
--- -------------------------------------------------------------------------- --
---                                                                            --
---                                                        :::      ::::::::   --
---   options.lua                                        :+:      :+:    :+:   --
---                                                    +:+ +:+         +:+     --
---   By: nbardavi <nbardavi@student.42.fr>          +#+  +:+       +#+        --
---                                                +#+#+#+#+#+   +#+           --
---   Created: 2025/01/15 09:50:10 by nbardavi          #+#    #+#             --
---   Updated: 2025/03/12 14:41:51 by nbardavi         ###   ########.fr       --
---                                                                            --
--- -------------------------------------------------------------------------- --
-
 vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
 
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-    callback = function()
-        local hl = vim.api.nvim_get_hl(0, {name="CursorLine"})
-        vim.api.nvim_set_hl(0, "CursorColumn", {bg=hl.bg});
-        vim.wo.cursorline = true
-        vim.wo.cursorcolumn = true
-    end,
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, {name="CursorLine"})
+    vim.api.nvim_set_hl(0, "CursorColumn", {bg=hl.bg});
+    vim.wo.cursorline = true
+    vim.wo.cursorcolumn = true
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-    callback = function()
-        vim.wo.cursorline = false
-        vim.wo.cursorcolumn = false
-    end,
+  callback = function()
+    vim.wo.cursorline = false
+    vim.wo.cursorcolumn = false
+  end,
 })
 
 opt.relativenumber = true
 opt.number = true
 
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
-opt.wrap = false
+opt.wrap = true
 
 opt.ignorecase = true
 opt.smartcase = true
--- lua/nbardavid/plugins/snacks.lua
+++ lua/nbardavid/plugins/snacks.lua
@@ -1,12 +1,17 @@
 return {
-    "folke/snacks.nvim",
-    opts = {
-        picker = {},
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = { exclude = { "node_modules", "generated" } },
+        grep = { exclude = { "node_modules", "generated"} },
+      },
     },
-    keys = {
-        { "<leader>ff", function() require("snacks.picker").files() end, desc = "Find files" },
-        { "<leader>fm", function() require("snacks.picker").smart() end, desc = "Find files" },
-        { "<leader>fw", function() require("snacks.picker").grep() end, desc = "Find Words" },
-        { "<leader>fc", function() require("snacks.picker").files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    },
+  },
+  keys = {
+    { "<leader>ff", function() require("snacks.picker").files() end, desc = "Find files" },
+    { "<leader>fm", function() require("snacks.picker").smart() end, desc = "Find files" },
+    { "<leader>fw", function() require("snacks.picker").grep() end, desc = "Find Words" },
+    { "<leader>fc", function() require("snacks.picker").files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+  },
 }
