78f5c8732b11f4490e2b9cf3d8e19f469e57b027
Nathan BARDAVID <nathan.bardavid@rainpath.com>
2026-04-14 14:40:50 +0200

    update plugins, rebind diffview keys, pin treesitter master

--- lua/nbardavid/plugins/diffview.lua
+++ lua/nbardavid/plugins/diffview.lua
@@ -1,9 +1,9 @@
 return {
     "sindrets/diffview.nvim",
     keys = {
-        { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+        { "<leader>gs", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
         { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
         { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
-        { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+        { "<leader>gx", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
     },
 }
