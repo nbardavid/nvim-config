180547abdad5a0dd6ff8e12063781299fa74cd02
Nathan BARDAVID <nathan.bardavid@rainpath.com>
2026-04-14 10:32:44 +0200

    add diffview.nvim

---
+++ lua/nbardavid/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+    "sindrets/diffview.nvim",
+    keys = {
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Branch History" },
+        { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+    },
+}
